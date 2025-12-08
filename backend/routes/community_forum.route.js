7337177c3dbeb41d54b08f57c1c3c98238518cff
Shreedeep Datta <shreedeepdatta2@gmail.com>
2025-12-08 14:10:03 +0530

    COmmunity forum likes added

--- backend/routes/community_forum.route.js
+++ backend/routes/community_forum.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { add_comment_to_forum, create_community_forum, delete_comment, downvote_comment, get_community_form_by_id, get_community_forum, upvote_comment } from "../controllers/community_forum.controller"
+import { add_comment_to_forum, create_community_forum, delete_comment, dislike_community, get_community_form_by_id, get_community_forum, like_community} from "../controllers/community_forum.controller"
 import { protectRoute, supportOrAdminRoute } from "../middleware/auth.middleware"
 
 const router = express.Router()
@@ -9,6 +9,6 @@ router.get("/get_communtiy_form_by_id/:id", get_community_form_by_id)
 router.post("/add_community_forum", supportOrAdminRoute, create_community_forum)
 router.delete("/delete_comment", supportOrAdminRoute, delete_comment)
 router.post("/add_comment", protectRoute, add_comment_to_forum)
-router.put("/upvote", protectRoute, upvote_comment)
-router.put("/downvote", protectRoute, downvote_comment)
+router.put("/upvote", protectRoute, like_community)
+router.put("/downvote", protectRoute, dislike_community)
 router.delete("/comment", protectRoute, delete_comment)
